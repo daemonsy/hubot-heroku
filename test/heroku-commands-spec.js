9d5b64012b1bcdc33349b8e4281c112afc428b6f
Damon Aw <daemonsy@gmail.com>
2017-04-21 23:52:42 -0400

    Get rid of waitForReplies. Woohoo! One less homebrewed method

--- test/heroku-commands-spec.js
+++ test/heroku-commands-spec.js
@@ -9,29 +9,19 @@ process.env.HUBOT_HEROKU_API_KEY = 'fake_key';
 process.env.HUBOT_AUTH_ADMIN = 1;
 let { expect } = chai;
 
-var waitForReplies = (number, room, callback) =>
-  setTimeout(function() {
-    if (room && (room.messages.length >= number)) {
-      return callback(room);
-    } else {
-      return waitForReplies(number, room, callback);
-    }
-  })
-;
-
-describe("Heroku Commands", function() {
+describe("Heroku Commands", () => {
   let helper = new HubotHelper("../index.coffee");
   let room = null;
   let mockHeroku = nock("https://api.heroku.com");
 
   beforeEach(() => room = helper.createRoom());
 
-  afterEach(function() {
+  afterEach(() => {
     room.destroy();
     nock.cleanAll();
   });
 
-  it("exposes help commands", function() {
+  it("exposes help commands", () => {
     let commands = room.robot.commands.filter(command => command.slice(0,12) === "hubot heroku");
 
     expect(commands).to.have.length(12);
@@ -50,180 +40,161 @@ describe("Heroku Commands", function() {
     expect(commands).to.include("hubot heroku ps:scale <app> <type>=<size>(:<quantity>) - Scales dyno quantity up or down");
   });
 
-  describe("heroku list apps <app name>", function() {
+  describe("heroku list apps <app name>", () => {
     beforeEach(() =>
       mockHeroku
         .get("/apps")
         .replyWithFile(200, __dirname + "/fixtures/app-list.json")
     );
 
-    describe("when given an argument <app name>", () =>
-      it("returns a list of the apps filtered by <app name>", function(done) {
-        room.user.say("Damon", "hubot heroku list apps staging");
-
-        return waitForReplies(3, room, function() {
+    describe("when given an argument <app name>", done => {
+      it("returns a list of the apps filtered by <app name>", done => {
+        room.user.say("Damon", "hubot heroku list apps staging").then(() => {
           expect(room.messages[1][1]).to.equal("@Damon Listing apps matching: staging");
           expect(room.messages[2][1]).to.not.contain(/shield-global-watch\b/);
           expect(room.messages[2][1]).to.contain("shield-global-watch-staging");
 
-          return done();
+          done();
         });
-      })
-    );
+      });
+    });
 
-    return describe("when the command is called without arguments" , () =>
-      it("returns a list of all apps", function(done) {
-        room.user.say("Damon", "hubot heroku list apps");
-
-        return waitForReplies(3, room, function() {
+    describe("when the command is called without arguments" , done => {
+      it("returns a list of all apps", done => {
+        room.user.say("Damon", "hubot heroku list apps").then(() => {
           expect(room.messages[1][1]).to.equal("@Damon Listing all apps available...");
           expect(room.messages[2][1]).to.match(/shield-global-watch\b/);
           expect(room.messages[2][1]).to.contain("shield-global-watch-staging");
 
-          return done();
+          done();
         });
       })
-    );
+    });
   });
 
 
-  describe("heroku info <app>", () =>
-    it("gets information about the app's dynos", function(done) {
+  describe("heroku info <app>", () => {
+    it("gets information about the app's dynos", done => {
       mockHeroku
         .get("/apps/shield-global-watch")
         .replyWithFile(200, __dirname + "/fixtures/app-info.json");
 
-      room.user.say("Damon", "hubot heroku info shield-global-watch");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku info shield-global-watch").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Getting information about shield-global-watch");
         expect(room.messages[2][1]).to.contain("last_release : 2014-12-12T02:16:59Z");
-        return done();
+
+        done();
       });
     })
-  );
+  });
 
 
-  describe("heroku dynos <app>", () =>
-    it("lists all dynos and their status", function(done) {
+  describe("heroku dynos <app>", () => {
+    it("lists all dynos and their status", done => {
       mockHeroku
         .get("/apps/shield-global-watch/dynos")
         .replyWithFile(200, __dirname + "/fixtures/dynos.json");
 
-      room.user.say("Damon", "hubot heroku dynos shield-global-watch");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku dynos shield-global-watch").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Getting dynos of shield-global-watch");
         expect(room.messages[2][1]).to.include("@Damon Dynos of shield-global-watch\n=== web (1X): `forever server.js`\nweb.1: up 2015/01/01 12:00:00");
         expect(room.messages[2][1]).to.include("\nweb.2: crashed 2015/01/01 12:00:00");
         expect(room.messages[2][1]).to.include("\n\n=== worker (2X): `celery worker`\nworker.1: up 2015/06/01 12:00:00");
 
-        return done();
+        done();
       });
-    })
-  );
+    });
+  });
 
-  describe("heroku releases <app>", () =>
-    it("gets the 10 recent releases", function(done) {
+  describe("heroku releases <app>", () => {
+    it("gets the 10 recent releases", done => {
       mockHeroku
         .get("/apps/shield-global-watch/releases")
         .replyWithFile(200, __dirname + "/fixtures/releases.json");
 
-      room.user.say("Damon", "hubot heroku releases shield-global-watch");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku releases shield-global-watch").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Getting releases for shield-global-watch");
         expect(room.messages[2][1]).to.include("@Damon Recent releases of shield-global-watch\nv352 - Promote shield-global-watch v287 fb2b5ff - phil@shield.com");
 
-        return done();
+        done();
       });
     })
-  );
+  });
 
 
-  describe("heroku rollback <app> <version>", function() {
-    beforeEach(function() {
+  describe("heroku rollback <app> <version>", () => {
+    beforeEach(() => {
       mockHeroku
         .get("/apps/shield-global-watch/releases")
         .replyWithFile(200, __dirname + "/fixtures/releases.json");
 
-      return mockHeroku
+      mockHeroku
         .post('/apps/shield-global-watch/releases')
         .replyWithFile(200,  __dirname + "/fixtures/rollback.json");
     });
 
-    it("rolls back the app to the specified version", function(done) {
+    it("rolls back the app to the specified version", done => {
       room.user.say("Damon", "hubot heroku rollback shield-global-watch v352").then(() => {
-        waitForReplies(3, room, function() {
-          expect(room.messages[1][1]).to.equal("@Damon Rolling back to v352");
-          expect(room.messages[2][1]).to.equal("@Damon Success! v353 -> Rollback to v352");
+        expect(room.messages[1][1]).to.equal("@Damon Rolling back to v352");
+        expect(room.messages[2][1]).to.equal("@Damon Success! v353 -> Rollback to v352");
 
-          return done();
-        });
+        done();
       });
     });
 
-    return it("tells the user about a bad supplied version", function(done) {
-      room.user.say("Damon", "hubot heroku rollback shield-global-watch v999");
-
-      return waitForReplies(3, room, function() {
+    it("tells the user about a bad supplied version", done => {
+      room.user.say("Damon", "hubot heroku rollback shield-global-watch v999").then(() => {
         expect(room.messages[1][1]).to.match(/v999/);
         expect(room.messages[2][1]).to.equal("@Damon Version v999 not found for shield-global-watch :(");
 
-        return done();
+        done();
       });
     });
   });
 
-  describe("heroku restart <app> <dyno>", function() {
-    it("restarts the app", function(done) {
+  describe("heroku restart <app> <dyno>", () => {
+    it("restarts the app", done => {
       mockHeroku
         .delete("/apps/shield-global-watch/dynos")
         .reply(200, {});
 
-      room.user.say("Damon", "hubot heroku restart shield-global-watch");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku restart shield-global-watch").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Telling Heroku to restart shield-global-watch");
         expect(room.messages[2][1]).to.equal("@Damon Heroku: Restarting shield-global-watch");
 
-        return done();
+        done();
       });
     });
 
-    it("restarts all dynos of a process", function(done) {
+    it("restarts all dynos of a process", done => {
       mockHeroku
         .delete("/apps/shield-global-watch/dynos/web")
         .reply(200, {});
 
-      room.user.say("Damon", "hubot heroku restart shield-global-watch web");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku restart shield-global-watch web").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Telling Heroku to restart shield-global-watch web");
         expect(room.messages[2][1]).to.equal("@Damon Heroku: Restarting shield-global-watch web");
 
-        return done();
+        done();
       });
     });
 
-    return it("restarts specific dynos", function(done) {
+    return it("restarts specific dynos", done => {
       mockHeroku
         .delete("/apps/shield-global-watch/dynos/web.1")
         .reply(200, {});
 
-      room.user.say("Damon", "hubot heroku restart shield-global-watch web.1");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku restart shield-global-watch web.1").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Telling Heroku to restart shield-global-watch web.1");
         expect(room.messages[2][1]).to.equal("@Damon Heroku: Restarting shield-global-watch web.1");
 
-        return done();
+        done();
       });
     });
   });
 
-  describe("heroku migrate <app>", function() {
-    beforeEach(function() {
+  describe("heroku migrate <app>", () => {
+    beforeEach(() => {
       mockHeroku
         .post("/apps/shield-global-watch/dynos", {
           command: "rake db:migrate",
@@ -256,44 +227,40 @@ describe("Heroku Commands", function() {
     });
   });
 
-  describe("heroku config <app>", () =>
-    it("gets a list of config keys without values", function(done) {
+  describe("heroku config <app>", () => {
+    it("gets a list of config keys without values", done => {
       mockHeroku
         .get("/apps/shield-global-watch/config-vars")
         .replyWithFile(200, __dirname + "/fixtures/config.json");
 
-      room.user.say("Damon", "hubot heroku config shield-global-watch");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config shield-global-watch").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Getting config keys for shield-global-watch");
         expect(room.messages[2][1]).to.equal("@Damon CLOAK, COMMANDER, AUTOPILOT, PILOT_NAME");
 
-        return done();
+        done();
       });
-    })
-  );
+    });
+  });
 
-  describe("heroku config:set <app> <KEY=value>", function() {
+  describe("heroku config:set <app> <KEY=value>", () => {
     let mockRequest = keyPair =>
       mockHeroku
         .patch("/apps/shield-global-watch/config-vars",keyPair)
         .replyWithFile(200, __dirname + "/fixtures/config-set.json")
     ;
 
-    it("sets config <KEY=value>", function(done) {
+    it("sets config <KEY=value>", done => {
       mockRequest({ "CLOAK_ID": "example.com" });
 
-      room.user.say("Damon", "hubot heroku config:set shield-global-watch CLOAK_ID=example.com");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config:set shield-global-watch CLOAK_ID=example.com").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Setting config CLOAK_ID => example.com");
         expect(room.messages[2][1]).to.equal("@Damon Heroku: CLOAK_ID is set to example.com");
 
-        return done();
+        done();
       });
     });
 
-    it("handles UUIDs", function(done) {
+    it("handles UUIDs", done => {
       mockRequest({ "UUID": "d5126f0d-b3be-46af-883f-b330a73964f9" });
 
       room.user.say("Damon", "hubot heroku config:set shield-global-watch UUID=d5126f0d-b3be-46af-883f-b330a73964f9").then(() =>{
@@ -303,74 +270,64 @@ describe("Heroku Commands", function() {
       });
     });
 
-    it("handles URLs", function(done) {
+    it("handles URLs", done => {
       mockRequest({"PUSHER_URL": "http://c0d9g8najfcc4634kd3:cf2eeas0d8dghfa847d@api.pusherapp.com/apps/1234"});
 
-      room.user.say("Damon", "hubot heroku config:set shield-global-watch PUSHER_URL=http://c0d9g8najfcc4634kd3:cf2eeas0d8dghfa847d@api.pusherapp.com/apps/1234");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config:set shield-global-watch PUSHER_URL=http://c0d9g8najfcc4634kd3:cf2eeas0d8dghfa847d@api.pusherapp.com/apps/1234").then(() => {
         expect(room.messages[2][1]).to.equal("@Damon Heroku: PUSHER_URL is set to http://c0d9g8najfcc4634kd3:cf2eeas0d8dghfa847d@api.pusherapp.com/apps/1234");
 
-        return done();
+        done();
       });
     });
 
-    it("handles comma delimited strings", function(done) {
-      mockRequest({"COMMA_DELIMITED_STRING": "MiD,DA,MDe"});
+    it("handles comma delimited strings", done => {
+      mockRequest({ "COMMA_DELIMITED_STRING": "MiD,DA,MDe" });
 
-      room.user.say("Damon", "hubot heroku config:set shield-global-watch COMMA_DELIMITED_STRING=MiD,DA,MDe");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config:set shield-global-watch COMMA_DELIMITED_STRING=MiD,DA,MDe").then(() => {
         expect(room.messages[2][1]).to.equal("@Damon Heroku: COMMA_DELIMITED_STRING is set to MiD,DA,MDe");
 
-        return done();
+        done();
       });
     });
 
-    it("handles text strings", function(done) {
+    it("handles text strings", done => {
       mockRequest({"SENTENCE": "Don\'t stop believin."});
 
-      room.user.say("Damon", "hubot heroku config:set shield-global-watch SENTENCE=\"Don't stop believin.\"");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config:set shield-global-watch SENTENCE=\"Don't stop believin.\"").then(() => {
         expect(room.messages[2][1]).to.equal("@Damon Heroku: SENTENCE is set to Don\'t stop believin.");
 
-        return done();
+        done();
       });
     });
 
-    return it("handles RSA secret keys", function(done) {
+    return it("handles RSA secret keys", done => {
       mockRequest({"RSA_SECRET_KEY": "----BEGIN RSA PRIVATE KEY-----\nsfsdfdssfdsFDSFDGSDfsdfsfs\nSDfSDFdUbOfFRocKsSFDSFSDFDS=\n-----END RSA PRIVATE KEY-----\n"});
 
-      room.user.say("Damon", "hubot heroku config:set shield-global-watch RSA_SECRET_KEY=\"----BEGIN RSA PRIVATE KEY-----\nsfsdfdssfdsFDSFDGSDfsdfsfs\nSDfSDFdUbOfFRocKsSFDSFSDFDS=\n-----END RSA PRIVATE KEY-----\n\"");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config:set shield-global-watch RSA_SECRET_KEY=\"----BEGIN RSA PRIVATE KEY-----\nsfsdfdssfdsFDSFDGSDfsdfsfs\nSDfSDFdUbOfFRocKsSFDSFSDFDS=\n-----END RSA PRIVATE KEY-----\n\"").then(() => {
         expect(room.messages[2][1]).to.equal("@Damon Heroku: RSA_SECRET_KEY is set to \"----BEGIN RSA PRIVATE KEY-----\nsfsdfdssfdsFDSFDGSDfsdfsfs\nSDfSDFdUbOfFRocKsSFDSFSDFDS=\n-----END RSA PRIVATE KEY-----\n\"");
 
-        return done();
+        done();
       });
     });
   });
 
   describe("heroku config:unset <KEY>", () =>
-    it("unsets config <KEY>", function(done) {
+    it("unsets config <KEY>", done => {
       mockHeroku
         .patch("/apps/shield-global-watch/config-vars",
           {"CLOAK_ID": null}
         ).reply(200, {});
 
-      room.user.say("Damon", "hubot heroku config:unset shield-global-watch CLOAK_ID");
-
-      return waitForReplies(3, room, function() {
+      room.user.say("Damon", "hubot heroku config:unset shield-global-watch CLOAK_ID").then(() => {
         expect(room.messages[1][1]).to.equal("@Damon Unsetting config CLOAK_ID");
         expect(room.messages[2][1]).to.equal("@Damon Heroku: CLOAK_ID has been unset");
 
-        return done();
+        done();
       });
     })
   );
 
-  describe("heroku run rake <app>", function() {
+  describe("heroku run rake <app>", () => {
     beforeEach(() => {
       mockHeroku
         .post("/apps/shield-global-watch/dynos", {
@@ -404,20 +361,18 @@ describe("Heroku Commands", function() {
     });
   });
 
-  describe("heroku ps:scale", function() {
+  describe("heroku ps:scale", () => {
     let mockRequest = formation =>
       mockHeroku
         .patch("/apps/shield-global-watch/formation/web", formation)
         .replyWithFile(200, __dirname + "/fixtures/ps-scale.json");
 
-    it("scales dynos", function() {
-      mockRequest({quantity: "2"});
+    it("scales dynos", done => {
+      mockRequest({ quantity: "2" });
 
-      room.user.say("Damon", "hubot heroku ps:scale shield-global-watch web=2");
-
-      return waitForReplies(3, room, function() {
-        expect(room.messages[1][1]).to(equal("@Damon Telling Heroku to scale web dynos of shield-global-watch"));
-        expect(room.messages[2][1]).to(equal("@Damon Heroku: now running web at 2:standard-2X"));
+      room.user.say("Damon", "hubot heroku ps:scale shield-global-watch web=2").then(() => {
+        expect(room.messages[1][1]).to.equal("@Damon Telling Heroku to scale web dynos of shield-global-watch");
+        expect(room.messages[2][1]).to.equal("@Damon Heroku: now running web at 2:standard-2X");
 
         done();
       });
